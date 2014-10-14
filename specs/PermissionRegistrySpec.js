53cfabfa9e226c411ae70df81ce680d5d0d1a496
James Sapara <james.sapara@gmail.com>
2014-10-14 15:56:05 -0700

    Access functions on PermissionRegistry to work with associated PermissionProvider's

--- specs/PermissionRegistrySpec.js
+++ specs/PermissionRegistrySpec.js
@@ -2,6 +2,8 @@
 
 var PermissionRegistry = require('../lib/PermissionRegistry');
 var PermissionProvider = require('../lib/PermissionProvider');
+var Models = require('../lib/models');
+var Errors = require('../lib/Errors');
 
 describe("PermissionRegistry", function() {
   var permissionRegistry, securityRegistry, permissionProvider;
@@ -31,14 +33,84 @@ describe("PermissionRegistry", function() {
       expect(permissionRegistry.providers.Event).not.toBe(undefined);
       expect(permissionRegistry.providers.Event.create).toBe(permissionProvider);
     });
-  });
 
-  it('adds new items to existing entry', function() {
+    it('adds new items to existing entry', function() {
       permissionProvider = new PermissionProvider('create');
       permissionRegistry.register('Event', [permissionProvider]);
       permissionProvider = new PermissionProvider('read');
       permissionRegistry.register('Event', [permissionProvider]);
       expect(permissionRegistry.providers.Event).not.toBe(undefined);
       expect(permissionRegistry.providers.Event.read).toBe(permissionProvider);
+    });
   });
+
+  describe('permissionsForResource', function() {
+
+    beforeEach(function() {
+      permissionRegistry.register('Event', [
+        new PermissionProvider('create'), new PermissionProvider('update')]
+      );
+    });
+
+    it('returns empty array with no matching Resource', function() {
+      expect(permissionRegistry.permissionsForResource('DoesNotExist')).toEqual([]);
+    });
+
+    it('returns array of string names for permissions', function() {
+      expect(permissionRegistry.permissionsForResource('Event')).toEqual(['create','update']);
+    });
+  });
+
+  describe('providersForResource', function() {
+    var create, update;
+    beforeEach(function() {
+      create = new PermissionProvider('create');
+      update = new PermissionProvider('update');
+      permissionRegistry.register('Event', [create, update]);
+    });
+
+    it('returns empty object for resource not present', function() {
+      expect(permissionRegistry.providersForResource('NotPresent')).toEqual({});
+    });
+
+    it('returns keyed object of providers', function() {
+      expect(permissionRegistry.providersForResource('Event')).toEqual({create: create, update: update});
+    });
+  });
+
+  describe('getPermission', function() {
+    var role, resource;
+
+    beforeEach(function() {
+      role = new Models.Role('admin');
+      resource = new Models.Resource('Event', 1);
+      permissionRegistry.register('Event', [
+          new PermissionProvider('create', {
+            getPermission: function(role, resource, cb) {
+              setImmediate(cb, null, new Models.Permission(true));
+            }
+          })
+        ]
+      );
+    });
+
+    it('returns Error if no resource matches', function(done) {
+      permissionRegistry.getPermission('update', role, new Models.Resource('Nope'), function(err, permission) {
+        expect(err).not.toBe(null);
+        expect(err instanceof Errors.PermissionArchitectPermissionProviderNotFound);
+        expect(err.message).toBe('Permission Provider for Resource Nope not found.');
+        done();
+      });
+    });
+
+    it('returns Permission instance', function(done) {
+      permissionRegistry.getPermission('create', role, new Models.Resource('Event'), function(err, permission) {
+        expect(err).toBe(null);
+        expect(permission instanceof Models.Permission).toBe(true);
+        expect(permission.granted).toBe(true);
+        done();
+      });
+    });
+  });
+
 });
